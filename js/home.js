ea190bb014cd225af6365be393b09749bf8c666f
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-02-03 14:14:38 -0300

    My Finans

---
+++ js/home.js
@@ -0,0 +1,184 @@
+const balance = document.querySelector('#balance')
+const revenue = document.querySelector('#revenue')
+const expenditure = document.querySelector('#expenditure')
+
+const formCad = document.querySelector('#form-cad')
+const btnCad = document.querySelector('#btn-cad')
+const btnCancel = document.querySelector('#btn-cancel')
+
+const revenueBtn = document.querySelector('#revenue-btn')
+const expenditureBtn = document.querySelector('#expenditure-btn')
+const value = document.querySelector('#value')
+const description = document.querySelector('#description')
+const category = document.querySelector('#category')
+const date = document.querySelector('#date')
+
+const ul = document.querySelector('#alert')
+
+// botão de receita
+revenueBtn.onclick = () => {
+    btnCad.classList.add('revenue-btn')
+    btnCad.classList.remove('expenditure-btn')
+
+    formCad.classList.add('show')
+    formCad.setAttribute('revenue', true)
+    formCad.removeAttribute('expenditure', true)
+}
+
+// botão de despesa
+expenditureBtn.onclick = () => {
+    btnCad.classList.add('expenditure-btn')
+    btnCad.classList.remove('revenue-btn')
+
+    formCad.classList.add('show')
+    formCad.setAttribute('expenditure', true)
+    formCad.removeAttribute('revenue', true)
+}
+
+// botão para cancelar
+btnCancel.onclick = e => {
+    e.preventDefault()
+
+    value.value = ''
+    description.value = ''
+    category.value = ''
+    date.value = ''
+}
+
+// setar id no localstorage
+function nextId() {
+    const id = localStorage.getItem('id')
+
+    if(id === null) {
+        localStorage.setItem('id', 0)
+    }
+  
+    return parseInt(localStorage.getItem('id')) + 1
+}
+// função factory
+function factoryTransactions(id, value, description, category, date) {
+    return {
+        id,
+        value, 
+        description,
+        category,
+        date
+    }
+}
+
+//setar transações
+function setTransactions() {
+    const id = nextId()
+    const valueV = value.value
+    const descriptionV = description.value
+    const categoryV = category.value
+    const dateValueUsa = date.value
+    const dateUsa = dateValueUsa.split('-')
+    const dateValue = `${dateUsa[2]}/${dateUsa[1]}/${dateUsa[0]}`
+
+    const expenditureTrue = document.querySelector('[expenditure]')
+    let revenueOrExpenditure = ''
+
+    // se for despesa
+    if(expenditureTrue) {
+        let transactions = factoryTransactions(
+            id,
+            -valueV,
+            descriptionV,
+            categoryV,
+            dateValue
+        )
+        
+        revenueOrExpenditure = 'Despesa'
+        localStorage.setItem(id, JSON.stringify(transactions))
+        localStorage.setItem('id', id)
+
+    // se for receita
+    } else {
+        let transactions = factoryTransactions(
+            id,
+            valueV,
+            descriptionV,
+            categoryV,
+            dateValue
+        )
+
+        revenueOrExpenditure = 'Saldo'
+        localStorage.setItem(id, JSON.stringify(transactions))
+        localStorage.setItem('id', id)
+    }
+
+    // verificar se foram setados os valores
+    if(valueV === '' || descriptionV === '' || categoryV === '' || dateValue === '') {
+        return alert('Preencha todos os campos para cadastrar sua transação')
+    }
+
+    //alert transação cadastrada
+    // const li = document.createElement('li')
+    // const li2 = document.createElement('li')
+
+    // li.classList.add('headLi')
+    // li2.classList.add('bodyLi')
+
+    // let valueN = Number(valueV).toFixed(2)
+
+    // li.innerText = `${revenueOrExpenditure} cadastrado(a) no dia ${dateValue}`
+    // li2.innerText = `${descriptionV} ${categoryV} R$${valueN}`
+
+    // ul.append(li)
+    // ul.append(li2)
+    // ul.classList.add('alert')
+}
+
+// todas as transações
+let arrayAlltransactions = []
+
+function alltransactions() {
+    const ids = localStorage.getItem('id')
+    
+    for(let i = 1; i <= ids; i++) {
+        const transactions = JSON.parse(localStorage.getItem(i))
+    
+        if(transactions === null) continue
+
+        arrayAlltransactions.push(transactions)
+
+    }
+
+    return arrayAlltransactions
+}
+
+alltransactions()
+
+//balanço
+function balanceMonth(v) {
+    const valuesAdd = v.map(e => parseInt(e.value))
+
+    const balanceMonth = valuesAdd
+    .reduce((a, val) => a + val, 0)
+    .toFixed(2)
+    balance.innerText = `R$ ${balanceMonth}`
+
+    const revenueMonth = valuesAdd
+    .filter(e => e > 0)
+    .reduce((a, val) => a + val, 0)
+    .toFixed(2)
+    revenue.innerText = `R$ ${revenueMonth}`
+
+    const expenditureMonth = valuesAdd
+    .filter(e => e < 0)
+    .reduce((a, val) => a + val, 0)
+    .toFixed(2)
+    expenditure.innerText = `R$ ${expenditureMonth}`
+}
+
+balanceMonth(arrayAlltransactions)
+
+//submetendo formulario
+formCad.addEventListener('submit', e => {
+    e.preventDefault()
+
+    setTransactions()
+    balanceMonth(arrayAlltransactions)
+    location.reload()
+})
---
+++ js/transactions.js
@@ -0,0 +1,163 @@
+const currentData = new Date
+const currentYear = currentData.getFullYear()
+const currentMonth = currentData.getMonth() + 1
+
+const ids = localStorage.getItem('id')
+const arrayTransactions = []
+
+const formDate = document.querySelector('#form-date')
+
+const yearFilter = document.querySelector('#year-filter')
+let yearOption = yearFilter
+
+const monthFilter = document.querySelector('#month-filter')
+let monthOption = monthFilter
+
+const transactionsYear = document.querySelector('#transactions-year')
+
+for(let a = currentYear; a <= (currentYear + 10); a++) {
+    let yearOption = document.createElement('option')
+    yearOption.innerHTML = a
+    yearFilter.append(yearOption)
+}
+
+switch(currentMonth) {
+    case 1: 
+        monthFilter.value = 1
+        break
+    case 2: 
+        monthFilter.value = 2
+        break
+    case 3: 
+        monthFilter.value = 3
+        break
+    case 4: 
+        monthFilter.value = 4
+        break
+    case 5: 
+        monthFilter.value = 5
+        break
+    case 6: 
+        monthFilter.value = 6
+        break
+    case 7: 
+        monthFilter.value = 7
+        break
+    case 8: 
+        monthFilter.value = 8
+        break
+    case 9: 
+        monthFilter.value = 9
+        break
+    case 10: 
+        monthFilter.value = 10
+        break
+    case 11: 
+        monthFilter.value = 11
+        break
+    case 12: 
+        monthFilter.value = 12
+        break
+}
+
+for(let i = 1; i <= ids; i++) {
+    const transactions = JSON.parse(localStorage.getItem(i))
+
+    if(transactions === null) continue
+
+    arrayTransactions.push(transactions)
+}
+
+function showTransactions(y, m) {
+    
+    arrayTransactions.map(e => {
+
+        const date = e.date
+        const dateSplit = date.split('/')
+        const year = dateSplit[2]
+        const monthNumber = dateSplit[1]
+        let month = ''
+        
+        switch(monthNumber) {
+            case '01':
+                month = 'Janeiro'
+                break
+            case '02':
+                month = 'Fevereiro'
+                break
+            case '03':
+                month = 'março'
+                break
+            case '04':
+                month = 'Abril'
+                break
+            case '05':
+                month = 'Maio'
+                break
+            case '06':
+                month = 'Junho'
+                break
+            case '07':
+                month = 'Julho'
+                break
+            case '08':
+                month = 'Agosto'
+                break
+            case '09':
+                month = 'Setembro'
+                break
+            case '10':
+                month = 'Outubro'
+                break
+            case '11':
+                month = 'Novembro'
+                break
+            case '12':
+                month = 'Dezembro'
+                break
+        }
+
+        if(y === year && `0${m}` == monthNumber) {
+
+            const li = document.createElement('li')
+            li.classList.add('transation-li')
+
+            // remover transations
+            const btnRemove = document.createElement('button')
+
+            btnRemove.setAttribute('id', `${e.id}`)
+            btnRemove.classList.add('delete')
+
+            btnRemove.onclick = () => {
+                localStorage.removeItem(e.id)
+                location.reload()
+            }
+
+            li.innerHTML = `
+            <span>${e.category}</span>
+            <span>${e.description}</span>
+            <span>R$ ${e.value}</span>
+            <button resolve class='solve'></button>
+            <button edite class='edit'></button>`
+            li.appendChild(btnRemove)
+
+            transactionsYear.append(li)
+        }
+
+    })
+}
+
+showTransactions(yearOption.value, monthOption.value)
+
+formDate.addEventListener('submit', e => {
+    e.preventDefault()
+
+    let yearOption = yearFilter
+    
+    transactionsYear.innerHTML = ''
+
+    yearOption.value = yearFilter.value 
+    monthOption.value = monthFilter.value
+
+    showTransactions(yearOption.value, monthOption.value)
+})
